d85724e99dff4607c8630191afe2e99cf6484f4d
mai vu <maivu629@gmail.com>
2021-05-16 19:25:49 +0700

    components

---
+++ src/components/AllUser/AllUser.js
@@ -0,0 +1,44 @@
+import React from "react";
+// @material-ui/core components
+// import { makeStyles } from "@material-ui/core/styles";
+import { DataGrid } from "@material-ui/data-grid";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  {
+    field: "role",
+    headerName: "Role",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.getValue("firstName") || ""} ${
+        params.getValue("lastName") || ""
+      }`,
+  },
+];
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", role: "supervisor" },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+];
+
+// const useStyles = makeStyles(styles);
+
+export default function UserProfile() {
+  // const classes = useStyles();
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+  );
+}
